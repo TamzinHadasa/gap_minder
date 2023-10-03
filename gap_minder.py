d5c6e6cfd4e49ab7aef978a55d7718aa68632221
Tamzin Hadasa Kelly <coding@tamz.in>
2023-10-03 04:10:45 -0400

    refactor as script

--- gap_minder.py
+++ gap_minder.py
@@ -1,40 +1,81 @@
-# %%
 import datetime as dt
-import requests
-from typing import Any
+from collections import UserList
+from typing import Any, Iterable, Literal, TypedDict
 
-# %%
-NonAutoDict = list[dict[str, str | int]]
+import requests
+
+
+class Data(TypedDict):
+    project: str
+    username: str
+    namespace: int | Literal['all']
+    limit: int
+    nonautomated_edits: 'ContribList'
+
+
+class Contrib(TypedDict):
+    full_page_title: str
+    page_title: str
+    namespace: int
+    rev_id: int
+    timestamp: dt.datetime
+    minor: int
+    length: int
+    length_change: int
+    comment: str
+    username: str
+
+
+ContribList = list[Contrib]
 HEADERS = {
     'User-Agent': 'edit timing analyzer [pre-alpha]',
     'From': 'wikimedian@tamz.in'
 }
+API_PATH = "https://xtools.wmcloud.org/api/user/nonautomated_edits/en.wikipedia"
 
-# %%
-class CombinedContribs(list):
+
+class Contribs(UserList):
+
+    @classmethod
+    def new_nonauto(cls, username: str, start: str = "", end: str = "", limit: int = 5000):
+        query_url = f"{API_PATH}/{username}/all/{start}/{end}?limit={limit}"
+        print("Querying XTools API...")
+        data: Data = requests.get(query_url, headers=HEADERS, timeout=90).json()
+        nonauto: ContribList = [
+            (edit | {'username': username,  # type: ignore
+                     'timestamp': dt.datetime.strptime(str(edit['timestamp']), "%Y%m%d%H%M%S")})
+            for edit in data['nonautomated_edits']
+        ]
+        obj = Contribs(nonauto)
+        return obj
+
+    def __add__(self, other: Iterable[Any]) -> 'Contribs':
+        try:
+            obj = Contribs(self.data + other.data)  # type: ignore
+        except AttributeError as e:
+            raise NotImplementedError from e
+        try:
+            obj.sort(key=lambda x: x['timestamp'])
+        except KeyError as e:
+            raise NotImplementedError from e
+        return obj
+
     def get_gaps(self) -> list[tuple[dt.timedelta, int, int]]:
         pairs = [(i, j) for i, j in zip(self, self[1:]) if i['username'] != j['username']]
-        print(f"{pairs=}")
         gaps = [(j['timestamp'] - i['timestamp'],
                  i['rev_id'],
                  j['rev_id'])
                 for i, j in pairs]
-        print(f"{gaps=}")
         gaps.sort(key=lambda x: x[0])
         return gaps
 
-# %%
-class UserContribs:
-    def __init__(self, username: str, start: str = "", end: str = "", limit: int = 5000):
-        query_url = f"https://xtools.wmcloud.org/api/user/nonautomated_edits/en.wikipedia/{username}/all/{start}/{end}?limit={limit}"
-        self._data = requests.get(query_url, headers=HEADERS).json()
-        self.username = self._data['username']
-        self.nonauto: list[dict[str, Any]] = self._data['nonautomated_edits']
-        for edit in self.nonauto:
-            edit['username'] = self.username
-            edit['timestamp'] = dt.datetime.strptime(str(edit['timestamp']), "%Y%m%d%H%M%S")
 
-    def __add__(self, other: 'UserContribs') -> CombinedContribs:
-        combined = self.nonauto + other.nonauto
-        combined.sort(key=lambda x: x['timestamp'])
-        return CombinedContribs(combined)
+def main():
+    user1 = Contribs.new_nonauto(input("User 1? "))
+    user2 = Contribs.new_nonauto(input("User 2? "))
+    combined = user1 + user2
+    print(combined.get_gaps())
+
+
+if __name__ == "__main__":
+    main()
